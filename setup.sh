bf6c61093bbcade824a1ceaeaa84e44257d3a307
Adarsh <83777943+theCode-Breaker@users.noreply.github.com>
2022-03-11 16:44:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ modprobe loop
 pacman -S --noconfirm --needed artools iso-profiles
 pacman -S --noconfirm --needed virtualbox virtualbox-host-dkms
 modprobe vboxdrv
-ln -sf "$(pwd)/artixiso/artools-workspace" ~/
-ln -sf "$(pwd)/config/artools" ~/.config/
+ln -sf "$(pwd)/artixiso/artools-workspace" ./
+mkdir ./.config
+ln -sf "$(pwd)/config/artools" ./config
